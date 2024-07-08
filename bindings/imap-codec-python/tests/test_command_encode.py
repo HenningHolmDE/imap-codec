4b48e26c467c3864a16d7106953cb57ebb0ed6f9
Henning Holm <git@henningholm.de>
2024-07-08 17:04:56 +0200

    Extract multi fragment message constant

--- bindings/imap-codec-python/tests/test_command_encode.py
+++ bindings/imap-codec-python/tests/test_command_encode.py
@@ -17,21 +17,20 @@ class TestCommandEncode(unittest.TestCase):
         self.assertIsInstance(encoded, Encoded)
         self.assertEqual(encoded.dump(), b"a NOOP\r\n")
 
+    _MULTI_FRAGMENT_MESSAGE = {
+        "tag": "A",
+        "body": {
+            "Login": {
+                "username": {"Atom": "alice"},
+                "password": {
+                    "String": {"Literal": {"data": list(b"\xCA\xFE"), "mode": "Sync"}}
+                },
+            }
+        },
+    }
+
     def test_multi_fragment_command(self):
-        message = {
-            "tag": "A",
-            "body": {
-                "Login": {
-                    "username": {"Atom": "alice"},
-                    "password": {
-                        "String": {
-                            "Literal": {"data": list(b"\xCA\xFE"), "mode": "Sync"}
-                        }
-                    },
-                }
-            },
-        }
-        encoded = CommandCodec.encode(message)
+        encoded = CommandCodec.encode(self._MULTI_FRAGMENT_MESSAGE)
         self.assertIsInstance(encoded, Encoded)
         fragments = list(encoded)
         self.assertEqual(
@@ -44,38 +43,12 @@ class TestCommandEncode(unittest.TestCase):
         )
 
     def test_multi_fragment_command_dump(self):
-        message = {
-            "tag": "A",
-            "body": {
-                "Login": {
-                    "username": {"Atom": "alice"},
-                    "password": {
-                        "String": {
-                            "Literal": {"data": list(b"\xCA\xFE"), "mode": "Sync"}
-                        }
-                    },
-                }
-            },
-        }
-        encoded = CommandCodec.encode(message)
+        encoded = CommandCodec.encode(self._MULTI_FRAGMENT_MESSAGE)
         self.assertIsInstance(encoded, Encoded)
         self.assertEqual(encoded.dump(), b"A LOGIN alice {2}\r\n\xCA\xFE\r\n")
 
     def test_multi_fragment_command_dump_remaining(self):
-        message = {
-            "tag": "A",
-            "body": {
-                "Login": {
-                    "username": {"Atom": "alice"},
-                    "password": {
-                        "String": {
-                            "Literal": {"data": list(b"\xCA\xFE"), "mode": "Sync"}
-                        }
-                    },
-                }
-            },
-        }
-        encoded = CommandCodec.encode(message)
+        encoded = CommandCodec.encode(self._MULTI_FRAGMENT_MESSAGE)
         self.assertIsInstance(encoded, Encoded)
         self.assertEqual(
             next(encoded), {"Line": {"data": list(b"A LOGIN alice {2}\r\n")}}
